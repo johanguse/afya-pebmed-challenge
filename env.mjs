42495693a53e0ebea0132313d59a6c558ce6d209
Johan Guse <johanguse@gmail.com>
2023-12-06 09:54:14 -0300

    fix: env verification on vercel

--- env.mjs
+++ env.mjs
@@ -3,11 +3,6 @@ import { z } from 'zod'
 import { createEnv } from '@t3-oss/env-nextjs'
 
 export const env = createEnv({
-  server: {
-    // This is optional because it's only used in development.
-    // See https://next-auth.js.org/deployment.
-    NEXT_PUBLIC_APP_URL: z.string().min(1),
-  },
   client: {
     NEXT_PUBLIC_SUBSCRIPTION_API_URL: z.string().url().min(1),
     NEXT_PUBLIC_OFFER_API_URL: z.string().url().min(1),
