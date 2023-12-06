0eb765566dc2e05ef0d682222696cb95353e03ab
Johan Guse <johanguse@gmail.com>
2023-12-06 11:29:32 -0300

    fix: env

--- config/site.ts
+++ config/site.ts
@@ -1,26 +1,16 @@
 import { SiteConfig } from 'index'
 
-if (!process.env.NEXT_PUBLIC_VERCEL_URL) {
-  throw new Error('Missing env.NEXT_PUBLIC_VERCEL_URL')
+if (!process.env.NEXT_PUBLIC_APP_URL) {
+  throw new Error('Missing env.NEXT_PUBLIC_APP_URL')
 }
 
-const site_url = process.env.NEXT_PUBLIC_VERCEL_URL.replace(/\/$/, '')
-
-export const getBaseUrl = (): string => {
-  switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
-    case 'production':
-    case 'preview':
-      return site_url
-    default:
-      return `http://localhost:${process.env.PORT || 3000}`
-  }
-}
+const site_url = process.env.NEXT_PUBLIC_APP_URL.replace(/\/$/, '')
 
 export const siteConfig: SiteConfig = {
   name: 'Front-end test PedMed Afya',
   description: 'Checkout app para testas os conhecimentos em front-end',
-  url: getBaseUrl(),
-  ogImage: `${getBaseUrl()}/og.jpg`,
+  url: site_url,
+  ogImage: `${site_url}/og.jpg`,
   links: {
     twitter: 'https://twitter.com/johanguse',
     github: 'https://github.com/johanguse/next-saas-template',
--- env.mjs
+++ env.mjs
@@ -4,12 +4,14 @@ import { createEnv } from '@t3-oss/env-nextjs'
 
 export const env = createEnv({
   client: {
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
     NEXT_PUBLIC_MOCK_API_URL: z
       .string()
       .url('ERRO: NEXT_PUBLIC_MOCK_API_URL not valid')
       .min(1),
   },
   runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_MOCK_API_URL: process.env.NEXT_PUBLIC_MOCK_API_URL,
   },
 })
