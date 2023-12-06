43e85e48b078da0a2398923ba0672ac081d98215
Johan Guse <johanguse@gmail.com>
2023-12-06 09:49:34 -0300

    fix: env verificatino on vercel

--- env.mjs
+++ env.mjs
@@ -6,7 +6,6 @@ export const env = createEnv({
   server: {
     // This is optional because it's only used in development.
     // See https://next-auth.js.org/deployment.
-    NEXTAUTH_URL: z.string().url().optional(),
     NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   client: {
@@ -14,8 +13,6 @@ export const env = createEnv({
     NEXT_PUBLIC_OFFER_API_URL: z.string().url().min(1),
   },
   runtimeEnv: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_SUBSCRIPTION_API_URL:
       process.env.NEXT_PUBLIC_SUBSCRIPTION_API_URL,
     NEXT_PUBLIC_OFFER_API_URL: process.env.NEXT_PUBLIC_OFFER_API_URL,
