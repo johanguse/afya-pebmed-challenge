980a5a1c9bbec3f73c0b4d575a7bf747554a3304
Johan Guse <johanguse@gmail.com>
2023-12-06 11:17:26 -0300

    fix: env

--- config/site.ts
+++ config/site.ts
@@ -6,11 +6,21 @@ if (!process.env.NEXT_PUBLIC_VERCEL_URL) {
 
 const site_url = process.env.NEXT_PUBLIC_VERCEL_URL.replace(/\/$/, '')
 
+export const getBaseUrl = (): string => {
+  switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+    case 'production':
+    case 'preview':
+      return site_url
+    default:
+      return `http://localhost:${process.env.PORT || 3000}`
+  }
+}
+
 export const siteConfig: SiteConfig = {
   name: 'Front-end test PedMed Afya',
   description: 'Checkout app para testas os conhecimentos em front-end',
-  url: site_url,
-  ogImage: `${site_url}/og.jpg`,
+  url: getBaseUrl(), // Call the function to get the base URL dynamically
+  ogImage: `${getBaseUrl()}/og.jpg`, // Ensuring ogImage uses the dynamic base URL
   links: {
     twitter: 'https://twitter.com/johanguse',
     github: 'https://github.com/johanguse/next-saas-template',
