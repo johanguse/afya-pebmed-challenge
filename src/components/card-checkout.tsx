38c36d0335b3bdab80328c1c5bec6c44cc35ff3a
Johan Guse <johanguse@gmail.com>
2023-12-06 10:19:03 -0300

    fix: env

--- src/components/card-checkout.tsx
+++ src/components/card-checkout.tsx
@@ -7,7 +7,7 @@ import { ISubscription } from '@/types/subscription'
 
 async function getCheckoutData() {
   try {
-    const url = `${process.env.NEXT_PUBLIC_MOCK_API_URL}/subscription`
+    const url = `https://private-0ced4-pebmeddesafiofrontend.apiary-mock.com/subscription`
 
     const res = await fetch(url, {
       cache: 'no-store',
